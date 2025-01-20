5b4171c9fc7359823ab4fec5629a03f06eae9542
Amrullah Azzaky <resuslampung@gmail.com>
2025-01-20 23:36:29 +0700

    script: menambahkan script hamburger menu pada script.js

--- script.js
+++ script.js
@@ -41,6 +41,17 @@ $(window).on('scroll', function() {
 
 });
 
+// hamburger menu script
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
+
+
+
 
 
 
